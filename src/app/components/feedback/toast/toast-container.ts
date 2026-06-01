1e32cd12ded9fb50da65b6e3b5e2dfca4adf3e1a
Shade <Shade@Shade.ga>
2026-06-01 20:25:01 +0400

    Increase toast right margin to avoid overlapping scrollbar

--- src/app/components/feedback/toast/toast-container.ts
+++ src/app/components/feedback/toast/toast-container.ts
@@ -17,10 +17,10 @@ export class ToastContainerComponent {
     const positions: Record<ToastPosition, string> = {
       'top-left': 'top-4 left-4 items-start',
       'top-center': 'top-4 left-1/2 -translate-x-1/2 items-center',
-      'top-right': 'top-4 right-4 items-end',
+      'top-right': 'top-4 right-6 items-end',
       'bottom-left': 'bottom-4 left-4 items-start',
       'bottom-center': 'bottom-4 left-1/2 -translate-x-1/2 items-center',
-      'bottom-right': 'bottom-4 right-4 items-end',
+      'bottom-right': 'bottom-4 right-6 items-end',
     };
     return `${base} ${positions[this.position()]}`;
   });
